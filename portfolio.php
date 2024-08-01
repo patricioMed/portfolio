c68486e79aa75a435380b8bc0084267d7ade8f84
patricioMed <137503149+patricioMed@users.noreply.github.com>
2024-08-02 07:56:24 +0800

    Add files via upload

---
+++ portfolio.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="portfolio1.css">
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <title>Portfolio.2</title>
+
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
+</head>
+<body>
+        <div class="navBar" id="home">
+            <h1 class="portfolio">Portfolio</h1>
+                <nav class="navs">
+                    <a class="home" href="portfolio.php">Home</a>
+                    <!-- <a class="home" href="#home">Home</a> -->
+                    <a class="about" href="#about">About</a>
+                    <a class="skills" href="#skills">Skills</a> 
+                    <a class="contact" href="#contact">Contact</a>
+                    <div class="class Animation"></div>
+                </nav>
+         </div>
+        <!-- </div> -->
+
+    <div class="mainContainer1">
+
+        <div class="leftSide1">
+            <div class="innerTextleft">
+            <!-- <h1 class="text1">Patrick Medrano</h1> -->
+            <!-- <h1 class="text2" >Back-end  Developer</h1> -->
+            <h1 class="animate__animated animate__fadeInDown" id="text1">Patrick Medrano</h1>
+            <h1 class="animate__animated animate__fadeInLeft" id="text2"><span class="autoTyped">Back-end Developer</span> </h1>
+            <p  class="animate__animated animate__fadeInUp" id="text3" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem labore molestias
+              aperiam nisi ea nemo ipsam nostrum laborum optio fugiat.
+            </p>
+            <div class="animate__animated animate__fadeInUp"  id="language">
+              <a class="app1" href=""><i class='bx bxl-python'></i></a>
+              <a class="app1" href=""><i class='bx bxl-java' ></i></a>
+              <a class="app1" href=""><i class='bx bxl-html5' ></i></a>
+              <a class="app1" href=""><i class='bx bxl-css3' ></i></a>
+              <a class="app1" href=""><i class='bx bxl-javascript' ></i></a>
+              <a class="app1" href=""><i class='bx bxl-php' ></i></a>
+            </div>
+
+
+
+
+            <!-- <h1 class="animate__animated animate__fadeInDown" id="text1">Patrick Medrano</h1>
+            <h1 class="animate__animated animate__fadeInLeft" id="text2"><span class="autoTyped">Back-end Developer</span> </h1>
+            <p  class="animate__animated animate__fadeInUp " id="text3" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem labore molestias
+              aperiam nisi ea nemo ipsam nostrum laborum optio fugiat.
+            </p> -->
+            </div>
+        </div>
+
+        <div class="rightSide1">
+           <div class="image">
+
+           </div> 
+        </div>
+        
+</div> <!-- mainContainer -->
+    
+    <div class="mainContainer2" id="about" >
+        <div class="leftSide2">
+        <div class="circle-container">
+      <div class="glowing-circle">
+ <div class="profile-pic"></div>
+    
+      </div>
+     </div>
+        </div>
+        <div class="rightSide2 ">
+<div class="rightText ">
+        <h1 class="aboutMe">ABOUT ME</h1>
+<!-- <h1 class="animate__animated animate__fadeInLeft" id="jobTitle">Backend Developer</h1> -->
+<h1 id="jobTitle">Backend Developer</h1>
+<p class="descripion">
+    <span>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+      Earum debitis facere laborum. Nisi aliquid deserunt doloribus issllo 
+      quod illum harum Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+      Tenetur sunt illo, cupiditate saepe voluptas natus ut expedita adipisci pariatur, 
+      eligendi in rerum! Iusto amet eius neque eligendi est error iste.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+      Earum debitis facere laborum. Nisi aliquid deserunt doloribus illo 
+      quod illum harum Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+      Tenetur sunt illo, cupiditate saepe voluptas natus ut expedita adipisci pariatur, 
+      eligendi in rerum! Iusto amet eius neque eligendi est error iste.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+      Earum debitis facere laborum. Nisi aliquid deserunt doloribus illo 
+      quod illum harum Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+      Tenetur sunt illo, cupiditate saepe voluptas natus ut expedita adipisci pariatur, 
+      eligendi in rerum! Iusto amet eius neque eligendi est error iste.
+     </span> 
+  </p>
+  </div>
+        </div>
+        </div> <!-- mainContainer2 -->
+
+    <!-- <div class="topText show-animate">
+            <h1 class="one" >TECHNICAL AND PROFESSIONAL</h1>
+    </div> -->
+    <div class="mainContainer3" id="skills">
+        <div class="leftSide3">
+            
+        <div class="skillLeft">
+  <h3 class="techPro" >Technical Skills</h3>
+
+<div class="skillBar">
+<div class="info">
+  <p>PYTHON</p>
+  <p class="percent">50%</p>
+</div>
+<div class="bar">
+    <span class="python"></span>
+</div>
+</div> <!-- skillBar -->
+
+<div class="skillBar">
+  <div class="info">
+    <p>JAVA</p>
+    <p class="percent">60%</p>
+  </div>
+<div class="bar">
+      <span class="java"></span>
+  </div>
+</div> <!-- skillBar -->
+
+<div class="skillBar">
+<div class="info">
+  <p>HTML</p>
+  <p class="percent">40%</p>
+</div>
+<div class="bar">
+    <span class="html"></span>
+</div>
+</div> <!-- skillBar -->
+<div class="skillBar"> 
+<div class="info"> 
+  <p>CSS</p>
+  <p class="percent">40%</p>
+</div>
+<div class="bar">
+    <span class="css"></span>
+</div>
+</div> <!-- skillBar -->
+<div class="skillBar">
+<div class="info">
+  <p>JAVASCRIPT</p>
+  <p class="percent">30%</p>
+</div>
+<div class="bar">
+    <span class="javascript"></span>
+</div>
+</div> <!-- skillBar -->
+<div class="skillBar">
+  <div class="info">
+    <p>PHP</p>
+    <p class="percent">20%</p>
+</div>
+  <div class="bar">
+      <span class="php"></span>
+  </div>
+  </div> <!-- skillBar -->
+
+  <div class="skillBar">
+  <div class="info">
+    <p>MYSQL</p>
+    <p class="percent">40%</p>
+</div>
+  <div class="bar">
+      <span class="mysql"></span>
+  </div>
+  </div> <!-- skillBar -->
+</div> <!-- skillLeft -->
+</div>
+
+        <div class="rightSide3">
+    <h3 class="proTech"> Professional Skills</h3>
+  <div class="prfssnl">
+    <div class="container">
+    <div class="inner-circle-container" dataDots="75" dataPercent="95">
+        <!-- <div class="points" style="--i:1;"></div>
+        <div class="points" style="--i:2;"></div>
+        <div class="points" style="--i:3;"></div> -->
+    </div>
+<div class="innerText">
+  <h5>95%</h5>
+  <h6>Team Work</h6>
+</div>
+    </div>
+    <div class="container">
+      <div class="inner-circle-container" dataDots="75" dataPercent="85">
+       <!-- <div class="points" style="--i:1;"></div>
+       <div class="points" style="--i:2;"></div>
+       <div class="points" style="--i:3;"></div> -->
+     </div>
+ <div class="innerText">
+   <h5>85%</h5>
+   <h6>Creativity</h6>
+ </div>
+     </div>
+
+     <div class="container">
+      <div class="inner-circle-container" dataDots="75" dataPercent="80">
+       <!-- <div class="points" style="--i:1;"></div>
+       <div class="points" style="--i:2;"></div>
+       <div class="points" style="--i:3;"></div> -->
+     </div>
+ <div class="innerText">
+   <h5>80%</h5>
+   <h6>Communication</h6>
+</div>
+     </div>
+
+     <div class="container">
+      <div class="inner-circle-container" dataDots="75" dataPercent="95">
+       <!-- <div class="points" style="--i:1;"></div>
+       <div class="points" style="--i:2;"></div>
+       <div class="points" style="--i:3;"></div> -->
+     </div>
+ <div class="innerText">
+   <h5>95%</h5>
+   <h6>Problem Solving</h6>
+ </div>
+     </div>
+
+  </div>
+        </div>
+    
+</div> <!-- mainContainer3 -->
+
+
+<div class="mainContainer4" id="contact">
+        
+        <div class="leftSide4">
+        <div class="containerLeft">
+        <h4 class="contactMe">Contact Me</h4>
+ <h4 class="letsWork">Let's Work Together</h4>
+ <p class="des">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+ Earum debitis facere laborum. Nisi aliquid 
+ deserunt doloribus illo quod illum harum.Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+ Earum debitis facere laborum. Nisi aliquid 
+ deserunt doloribus illo quod illum harum.
+ </p>
+
+ <div class="contacts">
+ <a class="app" href="#"><i class='bx bxl-facebook-square'></i></a>
+ <a class="app" href="#"><i class='bx bxl-github'></i></a>
+ <a class="app" href="#"><i class='bx bxl-linkedin-square'></i></a>
+ <a class="app" href="#"><i class='bx bxl-instagram-alt' ></i></a>
+ </div>
+
+ </div>
+
+        </div>
+        <div class="rightSide4">
+        <div class="form-container">
+ <div class="rightForm">
+ <input class="userInput" type="text" id="name" name="name" placeholder="Name" required>
+ <input class="userInput" type="email" id="name" name="name" placeholder="Email"required>
+ <input class="userInput" type="text" id="name" name="name" placeholder="Address"required>
+ <input class="userInput" ctype="text" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone Number" required>
+ <input class="userInput1" type="message" id="name" name="name" placeholder="Message" required>
+ </div>
+ <input class="submitButton" type="button" id="btn" name="name" value="Submit">
+ </div>
+        </div>
+  </div> <!-- mainContainer4 -->
+
+
+<script src="portfolio1.js"></script>
+    
+    <!-- <script>
+  const circles = document.querySelectorAll('.inner-circle-container'); 
+  circles.forEach(element=>{
+    var dots = element.getAttribute("dataDots");
+    var marked = element.getAttribute("dataPercent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+ 
+for(let i = 0 ; i < dots ; i++){
+  points += ` <div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+}
+element.innerHTML = points; 
+
+const pointsMarked = element.querySelectorAll('.points');
+
+for(let i = 0; i < percent; i++){
+  pointsMarked[i].classList.add('marked')
+}  
+
+})  
+</script> -->
+
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+<script>
+var myString = "Back-end Developer";
+var myValue = [];
+for (var i = 0; i < 1000000 ; i++) {
+  myValue.push(" ");
+  myValue.push(myString);
+}
+  var typed = new Typed(".autoTyped", {
+    // strings: ["Value1","Value2"], // example
+    strings:  myValue,
+    typeSpeed: 50,
+    backSpeed: 50,
+    looped: true,
+    startDelay: 2000, 
+    backDelay: 1000,
+    loopCount : Infinity
+  });
+</script>
+
+<script>
+const observer = new InterdivObserver((entries))=>{
+  entries.forEach((entries))=> {
+    console.log(entries)
+    if(entries.isIntersecting){
+      entries.target.classList.add('show');
+    }
+    else{
+      entries.target.classList.remove('show');
+    }
+  }
+}
+
+// const elements = document.querySelector('.animate__animated.animate__fadeInLeft', '.mainContainer2', '.mainContainer3', '.mainContainer4');
+const elements = document.getElementById('jobTitle');
+elements.forEach((element)) => observer.observe((element));
+</script>
+
+</body>
+</html>
